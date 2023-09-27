13d79f8ea736fa26365ca934d971ac1df130a018
Krishna  Jala <krishnamouli143@gmail.com>
2023-09-27 15:22:01 +0530

    After creating and uploading to s3 code

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
 import random
 
-
-# import boto3
+import boto3
 
 
 class ParkingArea:
-    def __init__(self, total_area, parking_size=(8, 12)):
-        self.parking_size = parking_size
-        self.num_of_places = total_area // (parking_size[0] * parking_size[1])
+    def __init__(self, total_area, wide=8, long=12):
+        self.wide = wide
+        self.long = long
+        self.num_of_places = total_area // (self.wide * self.long)
         self.parking_list = [None] * self.num_of_places
         self.parking_place_mapping_to_car = {}
 
@@ -27,7 +27,7 @@ class ParkingArea:
         return json.dumps(self.parking_place_mapping_to_car)
 
 
-# temp = ParkingArea(2000,(10,12))
+# temp = ParkingArea(2000,10,12)
 # print(temp.num_of_places)
 # print(temp.parking_list)
 
@@ -50,33 +50,64 @@ class Car:
 # new_car = Car("1234")
 # print(new_car)
 
+def upload_file_to_s3(file, path):
+    try:
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id="AWS_S3_KEY",
+            aws_secret_access_key="AWS_S3_SECRET"
+        )
+        key = path + str(random.choice("abcedfghijklmnopqrstuvwxyz0123456789"))
+        s3.upload_fileobj(
+            file,
+            "AWS_S3_TEST_BUCKET",
+            key
+        )
+        return path + key
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
 
-def main():
+
+def generate_cars(number_of_cars=20):
+    cars_list = {}
+    while len(cars_list) <= number_of_cars:
+        rand_number = str(random.randint(1000000, 9999999))
+        if rand_number not in cars_list:
+            cars_list[rand_number] = Car(rand_number)
+
+    return list(cars_list.values())
+
+
+def main(cars):
     parking_lot_size = 2000
     parking_lot = ParkingArea(parking_lot_size)
-
-    cars = [Car(str(random.randint(1000000, 9999999))) for _ in range(20)]
 
     for car in cars:
         while not parking_lot.is_parking_full():
             spot = random.randint(0, parking_lot.num_of_places - 1)
             if car.park(parking_lot, spot):
                 break
+        if parking_lot.is_parking_full():
+            print("Parking lot is full.")
+            break
 
-    if parking_lot.is_parking_full():
-        print("Parking lot is full.")
+    vehicle_to_spot_mapping_json = parking_lot.parking_details_json()
+    with open("vehicle_to_spot_mapping.json", "w") as file:
+        file.write(vehicle_to_spot_mapping_json)
+
 
 if __name__ == "__main__":
-    main()
+    main(generate_cars(10))
 
 # Parking Lot Challenge:
 #
-# Create a parking lot class that takes in a square footage size as input and creates an array of empty values based on the
-# input square footage size. Assume every parking spot is 8x12 (96 ft2) for this program, but have the algorithm that calculates
-# the array size be able to account for different parking spot sizes.
+# Create a parking lot class that takes in a square footage size as input and creates an array of empty values based on the input square footage size.
+# Assume every parking spot is 8x12 (96 ft2) for this program, but have the algorithm that calculates the array size be able to account for different parking spot sizes.
 #
-# For example, a parking lot of size 2000ft2 can fit 20 cars, but if the parking spots were 10x12 (120 ft2), it could only
-# fit 16 cars. The size of the array will determine how many cars can fit in the parking lot.
+# For example, a parking lot of size 2000ft2 can fit 20 cars, but if the parking spots were 10x12 (120 ft2), it could only fit 16 cars.
+# The size of the array will determine how many cars can fit in the parking lot.
+#
 #
 # Create a car class that takes in a 7 digit license plate and sets it as a property. The car will have 2 methods:
 #
